bdc8ec83412fbfe9b2cc1d3e93d79e1b36842c4c
Tim Gilevich <ouncetim@gmail.com>
2024-03-09 17:22:50 +0300

    Add project files.

---
+++ AddCppClass/AddCppClassPackage.cs
@@ -0,0 +1,22 @@
+﻿global using Community.VisualStudio.Toolkit;
+global using Microsoft.VisualStudio.Shell;
+global using System;
+global using Task = System.Threading.Tasks.Task;
+using Dwarfovich.AddCppClass;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace AddCppClass
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.AddCppClassString)]
+    public sealed class AddCppClassPackage : ToolkitPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.RegisterCommandsAsync();
+        }
+    }
+}
---
+++ AddCppClass/Commands/AddCppClassCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace Dwarfovich.AddCppClass
+{
+    [Command(PackageIds.AddCppClassCommand)]
+    internal sealed class AddCppClassCommand : BaseCommand<AddCppClassCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await VS.MessageBox.ShowWarningAsync("AddCppClass", "Button clicked");
+        }
+    }
+}
