5e2c498394c18426eb781ea5944e880b0d17bd01
Tim Gilevich <ouncetim@gmail.com>
2024-03-26 22:17:28 +0300

    WIP

--- AddCppClass/Commands/AddCppClassCommand.cs
+++ AddCppClass/Commands/AddCppClassCommand.cs
@@ -1,36 +1,11 @@
 ﻿using EnvDTE;
-using EnvDTE80;
-using Microsoft;
-using Microsoft.VisualStudio.Shell.Interop;
-using System.Diagnostics;
-
-using EnvDTE;
-
-using EnvDTE80;
-
-using Microsoft;
-using Microsoft.VisualStudio;
-using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Threading;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
 
 namespace Dwarfovich.AddCppClass
 {
     [Command(PackageIds.AddCppClassCommand)]
     internal sealed class AddCppClassCommand : BaseCommand<AddCppClassCommand>
     {
-        
         protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
         {
             //await VS.MessageBox.ShowWarningAsync("AddCppClass", "Button clicked");
@@ -38,8 +13,7 @@ namespace Dwarfovich.AddCppClass
             var dialog = new AddCppClassDialog();
             dialog.HasMinimizeButton = false;
             dialog.HasMaximizeButton = false;
-            dialog.ShowModal();
-            bool? result = dialog.DialogResult;
+            bool result = (bool)dialog.ShowModal();
         }
     }
 }
